a5fc3e8b96e967b97521c657327069ee30f35022
Spaceman1701 <fc.spaceman@gmail.com>
2016-12-04 04:24:54 -0700

    fix position bug

--- trends.py
+++ trends.py
@@ -7,10 +7,11 @@ from parse import load_sentiments
 import json
 from tweet import Tweet
 from colors import get_sentiment_color
+import ast
 
 
-TWEET_FILES = ["tweets_with_time.json", "tweets_with_time_2.json", "tweets_with_time_3.json", "tweets_with_time_4.json"]
-#TWEET_FILES = ["tweets.json", "tweets2.json", "tweets3.json", "tweets4.json", "tweets5.json"]
+#TWEET_FILES = ["tweets_with_time.json", "tweets_with_time_2.json", "tweets_with_time_3.json", "tweets_with_time_4.json"]
+TWEET_FILES = ["tweets.json", "tweets2.json", "tweets3.json", "tweets4.json", "tweets5.json"]
 DATA_LOCATION = "data" + os.sep
 
 
@@ -25,52 +26,48 @@ class SentimentAnalysis:
         self.usa = Country(self.states, 1200)
 
     def update_sentiments(self, query_list):
-        average_sent = 0
-        num = 0
         state_totals = dict()
         for tweet in self.tweets:
-            if all(query_word in tweet.message() for query_word in query_list):  # if every query word is in the tweet
-                sent = self.weigh_message(tweet.message())
+            if all(query_word.lower() in tweet.message().lower() for query_word in query_list):  # if every query word is in the tweet
+                sent = self.weigh_message(tweet.message().lower())
                 place = self.place_tweet(tweet.position())
                 if sent != -1:  # word was actually weighted
                     if place not in state_totals:
                         state_totals[place] = [0, 0]
-                    state_totals[place][0] += sent
-                    state_totals[place][1] += 1
-                    num += 1
+                    state_totals[place][0] += sent  # total sentiment value
+                    state_totals[place][1] += 1  # total number of tweets from state
 
         for state in self.states:
             if state.abbrev() in state_totals:
-                state_totals[state.abbrev()][0] /= float(state_totals[state.abbrev()][1])
-                self.usa.setFillColor(state.abbrev(), get_sentiment_color(state_totals[state.abbrev()][0]))
+                average_sent = state_totals[state.abbrev()][0] / state_totals[state.abbrev()][1]
+                self.usa.setFillColor(state.abbrev(), get_sentiment_color(average_sent))
                 print(state.abbrev(), state_totals[state.abbrev()][1])
             else:
                 self.usa.setFillColor(state.abbrev(), get_sentiment_color(None))
-        print num
+
     def place_tweet(self, position):
-        min_dist = 9999999999
+        min_dist = 999999999999  # a really big number
         close_state = None
-        geo_pos = GeoPosition(position[0], position[1])
+        geo_pos = GeoPosition(position[1], position[0])
         for state in self.states:
-            deltax = position[0] - state.centroid().longitude()
-            deltay = position[1] - state.centroid().latitude()
-            dist = deltax * deltax + deltay * deltay
+            dist = state.centroid().distance(geo_pos)
             if dist < min_dist:
                 min_dist = dist
                 close_state = state
 
-        return close_state.abbrev()
+        return close_state.abbrev()  # using abbrev as keys instead of reference makes debugging a bit easier
 
     def weigh_message(self, message):
         total = 0
-        split_str = message.split(' ')
+        split_str = message.split()
         num_words = 0
-        for word in self.sentiments:
-            if word in message:
+        for word in split_str:
+            if word in self.sentiments:
                 num_words += 1
                 total += self.sentiments[word]
         if not num_words:
             return -1
+        total /= num_words
 
         return total
 
@@ -88,9 +85,15 @@ def load_tweets():
 if __name__ == "__main__":
     query = None
     if len(sys.argv) > 1:
-        query = sys.argv[1:]
+        query = sys.argv[1]
     else:
-        query = ["bieber"]
+        print("error, searching with default query")
+        query = "['i']"
+
+    if query[0] =='[':  # if the user input a list
+        query = ast.literal_eval(query)
+    else:
+        query = [query]  # if the user input a string
 
     print query
 
